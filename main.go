c2b7a8bd9c92b18b1646db93aa5470c01bc22621
carlosghabrous <carlos.ghabrous@plantix.net>
2023-06-20 17:36:58 +0200

    CHORE: first commit

---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1>Welcome to my great site</h1>")
+}
+
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1>Contact Page</h1><p>email me at <a href=\"mailto:carlos.ghabrous@gmail.com\">carlos.ghabrous@gmail.com</a></p>")
+}
+
+// func pathHandler(w http.ResponseWriter, r *http.Request) {
+// 	switch r.URL.Path {
+// 	case "/":
+// 		homeHandler(w, r)
+// 	case "/contact":
+// 		contactHandler(w, r)
+// 	default:
+// 		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+// 		// notFoundHandler(w, r)
+// 	}
+// }
+
+// type Router struct{}
+
+// func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+// 	switch r.URL.Path {
+// 	case "/":
+// 		homeHandler(w, r)
+// 	case "/contact":
+// 		contactHandler(w, r)
+// 	default:
+// 		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+// 	}
+// }
+
+func main() {
+	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/contact", contactHandler)
+	fmt.Println("Starting the server on :3000...")
+	http.ListenAndServe(":3000", nil)
+}
