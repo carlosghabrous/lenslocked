679175961f2deb502a410a99c6b057cc06234363
carlosghabrous <carlos.ghabrous@plantix.net>
2023-09-11 07:33:58 +0200

    home page via template

--- main.go
+++ main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
+	"path"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
@@ -10,7 +13,20 @@ import (
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprint(w, "<h1>Welcome to my great site</h1>")
+	tpl, err := template.ParseFiles(path.Join("templates", "home.gohtml"))
+
+	if err != nil {
+		log.Printf("parsing template: %v", err)
+		http.Error(w, "error parsing the home template", http.StatusInternalServerError)
+		return
+	}
+
+	err = tpl.Execute(w, nil)
+	if err != nil {
+		log.Printf("executing template: %v", err)
+		http.Error(w, "error executing the home template", http.StatusInternalServerError)
+		return
+	}
 }
 
 func contactHandler(w http.ResponseWriter, r *http.Request) {
