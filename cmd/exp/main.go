b0e8d187aae958bf5d5bf9de3a8c15de622e8918
carlosghabrous <carlos.ghabrous@plantix.net>
2023-10-12 15:48:52 +0200

    play with fmt.Errorf

--- cmd/exp/main.go
+++ cmd/exp/main.go
@@ -1,38 +1,25 @@
 package main
 
 import (
-	"html/template"
-	"os"
+	"errors"
+	"fmt"
 )
 
-type User struct {
-	Name    string
-	Age     int
-	Weight  float64
-	Lottery []int
-	Phones  map[string]int
-	Meta    UserMeta
-}
-
-type UserMeta struct {
-	Visits      int
-	Conditional bool
-}
-
 func main() {
-	t, err := template.ParseFiles("hello.gohtml")
-	if err != nil {
-		panic(err)
-	}
+	err := CreateUser()
+	fmt.Println(err)
+}
 
-	// user := User{Name: "Carlos", Bio: `<script>alert("Ups");</script>`, Age: 123}
-	user := User{
-		Name: "carlos", Age: 453,
-		Weight: 68.2, Lottery: []int{1, 56, 26, 93, 2},
-		Phones: map[string]int{"carlos": 41456633},
-		Meta:   UserMeta{Visits: 4, Conditional: false}}
-	err = t.Execute(os.Stdout, user)
+func Connect() error {
+	return errors.New("could not connect")
+}
+
+func CreateUser() error {
+	err := Connect()
 	if err != nil {
-		panic(err)
+		return fmt.Errorf("create user: %w", err)
 	}
+	// ...
+
+	return nil
 }
