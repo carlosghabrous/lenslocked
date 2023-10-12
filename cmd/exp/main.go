f6ae81fa8ef7f1caee3f1b81d42ff5dc1cf2bc99
carlosghabrous <carlos.ghabrous@plantix.net>
2023-10-12 15:12:23 +0200

    experiments

--- cmd/exp/main.go
+++ cmd/exp/main.go
@@ -6,14 +6,17 @@ import (
 )
 
 type User struct {
-	Name string
-	Bio  string
-	Age  int
-	Meta UserMeta
+	Name    string
+	Age     int
+	Weight  float64
+	Lottery []int
+	Phones  map[string]int
+	Meta    UserMeta
 }
 
 type UserMeta struct {
-	Visits int
+	Visits      int
+	Conditional bool
 }
 
 func main() {
@@ -22,7 +25,12 @@ func main() {
 		panic(err)
 	}
 
-	user := User{Name: "Carlos", Bio: `<script>alert("Ups");</script>`, Age: 123}
+	// user := User{Name: "Carlos", Bio: `<script>alert("Ups");</script>`, Age: 123}
+	user := User{
+		Name: "carlos", Age: 453,
+		Weight: 68.2, Lottery: []int{1, 56, 26, 93, 2},
+		Phones: map[string]int{"carlos": 41456633},
+		Meta:   UserMeta{Visits: 4, Conditional: false}}
 	err = t.Execute(os.Stdout, user)
 	if err != nil {
 		panic(err)
