6d151dca89ff63ac8a3e56143697a813eee18095
carlosghabrous <carlos.ghabrous@google.com>
2025-02-17 20:10:56 +0100

    chore: process request arguments

--- main.go
+++ main.go
@@ -17,11 +17,19 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprint(w, "<h1>Contact</h1><p>Contact me <a href=\"mailto:carlos.ghabrous@gmail.com\">here</a></p>")
 }
+
+func getArticleById(w http.ResponseWriter, r *http.Request) {
+	articleID := chi.URLParam(r, "articleID")
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprintf(w, "<h1>Article ID: %s</h1>", articleID)
+}
+
 func main() {
 	fmt.Println("starting server at port :3000")
 	router := chi.NewRouter()
 	router.Use(middleware.Logger)
 	router.Get("/", homeHandler)
 	router.Get("/contact", contactHandler)
+	router.Get("/articles/{articleID}", getArticleById)
 	http.ListenAndServe(":3000", router)
 }
