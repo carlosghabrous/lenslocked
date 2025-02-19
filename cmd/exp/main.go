b3729e4d26cbf078879620972e2383e4b497c2c6
carlosghabrous <carlos.ghabrous@google.com>
2025-02-19 06:44:58 +0100

    chore: error experimentation

--- cmd/exp/main.go
+++ cmd/exp/main.go
@@ -1,30 +1,35 @@
 package main
 
 import (
-	"html/template"
-	"os"
+	"errors"
+	"fmt"
 )
 
-type Person struct {
-	Name      string
-	Age       int
-	Height    float32
-	Phone     []int
-	YearsCity map[int]string
-	Bio       string
+func main() {
+	err := CreateOrg()
+	fmt.Println(err)
 }
 
-func main() {
-	template, err := template.ParseFiles("hello.gohtml")
+func Connect() error {
+	return errors.New("connection error")
+}
 
+func CreateUser() error {
+	err := Connect()
 	if err != nil {
-		panic(err)
+		return fmt.Errorf("create user: %w", err)
+	}
+	// ...
+
+	return nil
+}
+
+func CreateOrg() error {
+	err := CreateUser()
+	if err != nil {
+		return fmt.Errorf("create org: %w", err)
+
 	}
 
-	carlos := Person{Name: "carlos", Age: 24, Height: 1.23, Phone: []int{1, 2, 3}, YearsCity: map[int]string{20: "Fuengirola", 15: "Geneva"}, Bio: "<script>alert('hacked')</script>"}
-
-	err = template.ExecuteTemplate(os.Stdout, "hello.gohtml", carlos)
-	if err != nil {
-		panic(err)
-	}
+	return nil
 }
