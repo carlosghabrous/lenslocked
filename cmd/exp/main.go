5d48341ed3cb2c6ded236eb255d391a951afa658
carlosghabrous <carlos.ghabrous@google.com>
2025-02-20 05:02:40 +0100

    chore: experiment with error types

--- cmd/exp/main.go
+++ cmd/exp/main.go
@@ -3,33 +3,15 @@ package main
 import (
 	"errors"
 	"fmt"
+	"os"
 )
 
 func main() {
-	err := CreateOrg()
-	fmt.Println(err)
-}
-
-func Connect() error {
-	return errors.New("connection error")
-}
-
-func CreateUser() error {
-	err := Connect()
-	if err != nil {
-		return fmt.Errorf("create user: %w", err)
+	if _, err := os.Open("non-existing-file"); err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			fmt.Println("File does not exist error")
+		} else {
+			fmt.Println("Non-permission error")
+		}
 	}
-	// ...
-
-	return nil
-}
-
-func CreateOrg() error {
-	err := CreateUser()
-	if err != nil {
-		return fmt.Errorf("create org: %w", err)
-
-	}
-
-	return nil
 }
