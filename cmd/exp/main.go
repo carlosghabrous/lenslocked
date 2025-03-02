5908c4d877db87cf884e2bc2cc7f5019387af371
carlosghabrous <carlos.ghabrous@google.com>
2025-03-02 17:00:02 +0100

    chore: config from struct

--- cmd/exp/main.go
+++ cmd/exp/main.go
@@ -1,17 +1,42 @@
 package main
 
 import (
-	"errors"
+	"database/sql"
 	"fmt"
-	"os"
+
+	_ "github.com/jackc/pgx/v4/stdlib"
 )
 
+type PostgresConfig struct {
+	Host     string
+	Port     string
+	User     string
+	Password string
+	Database string
+	SSLMode  string
+}
+
+func (cfg PostgresConfig) String() string {
+	return fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s", cfg.Host, cfg.Port, cfg.User, cfg.Password, cfg.Database, cfg.SSLMode)
+}
+
 func main() {
-	if _, err := os.Open("non-existing-file"); err != nil {
-		if errors.Is(err, os.ErrNotExist) {
-			fmt.Println("File does not exist error")
-		} else {
-			fmt.Println("Non-permission error")
-		}
+	cfg := PostgresConfig{
+		Host:     "localhost",
+		Port:     "5432",
+		User:     "carlos",
+		Password: "ghabrous",
+		Database: "lenslocked",
+		SSLMode:  "disable",
 	}
+	db, err := sql.Open("pgx", cfg.String())
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Connected to the database")
 }
