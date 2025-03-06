273bff9e3e87c95dedfb13a238b2471d8ae364bf
carlosghabrous <carlos.ghabrous@google.com>
2025-03-06 06:17:49 +0100

    chore: play with sql

--- cmd/exp/main.go
+++ cmd/exp/main.go
@@ -39,4 +39,33 @@ func main() {
 		panic(err)
 	}
 	fmt.Println("Connected to the database")
+	_, err = db.Exec(`
+    CREATE TABLE IF NOT EXISTS users (
+      id SERIAL PRIMARY KEY,
+      name TEXT,
+      email TEXT UNIQUE NOT NULL
+      );
+
+      CREATE TABLE IF NOT EXISTS orders (
+        id SERIAL PRIMARY KEY,
+        user_id INT NOT NULL,
+        amount INT,
+        description TEXT
+      );
+    `)
+
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Successfully created tables")
+	id := 23
+	row := db.QueryRow(`
+    SELECT name, email FROM users WHERE id = $1
+    `, id)
+	var name, email string
+	err = row.Scan(&name, &email)
+	if err == sql.ErrNoRows {
+		panic(err)
+	}
+	fmt.Printf("Name: %s, email: %s\n", name, email)
 }
