98a11f8dd53d0de9387c6f7b896fcd75aed32806
carlosghabrous <carlos.ghabrous@google.com>
2025-03-09 14:49:07 +0100

    chore: add postgres functionality

---
+++ models/postgres.go
@@ -0,0 +1,43 @@
+package models
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/jackc/pgx/v4/stdlib"
+)
+
+// Open will open a sql connection with the provided postgres db
+// Callers need to ensure the connection is eventually closed
+// via db.Close()
+func Open(config PostgresConfig) (*sql.DB, error) {
+	db, err := sql.Open("pgx", config.String())
+	if err != nil {
+		return nil, fmt.Errorf("error while opening db connection: %w", err)
+	}
+	return db, nil
+}
+
+func DefaultPostgresConfig() PostgresConfig {
+	return PostgresConfig{
+		Host:     "localhost",
+		Port:     "5432",
+		User:     "carlos",
+		Password: "ghabrous",
+		Database: "lenslocked",
+		SSLMode:  "disable",
+	}
+}
+
+type PostgresConfig struct {
+	Host     string
+	Port     string
+	User     string
+	Password string
+	Database string
+	SSLMode  string
+}
+
+func (cfg PostgresConfig) String() string {
+	return fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s", cfg.Host, cfg.Port, cfg.User, cfg.Password, cfg.Database, cfg.SSLMode)
+}
